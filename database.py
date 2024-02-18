706e845596c7d4c4f3284665bf5ca444865c72c5
Frunk Peng <46289416+Graves451@users.noreply.github.com>
2024-02-17 23:16:26 -0500

    good

--- database.py
+++ database.py
@@ -34,11 +34,11 @@ class database_conn:
 
     def _create_tables(self):
         try:
-            self.cur.execute("CREATE TABLE IF NOT EXISTS users_login (user_id SERIAL PRIMARY KEY, username VARCHAR(255) NOT NULL);")
-            self.cur.execute("CREATE TABLE IF NOT EXISTS users_info (user_id SERIAL PRIMARY KEY, level INTEGER NOT NULL, exp INTEGER NOT NULL);")
-            self.cur.execute("CREATE TABLE IF NOT EXISTS users_winrate (user_id SERIAL PRIMARY KEY, wins INTEGER NOT NULL, losses INTEGER NOT NULL, dpa FLOAT NOT NULL);")
-            self.cur.execute("CREATE TABLE IF NOT EXISTS users_interests (user_id INTEGER, interest VARCHAR(255) NOT NULL);")
-            self.cur.execute("CREATE TABLE IF NOT EXISTS users_elo (user_id SERIAL PRIMARY KEY, elo INTEGER NOT NULL);")
+            self.cur.execute("CREATE TABLE IF NOT EXISTS users_login (user_id VARCHAR(50) PRIMARY KEY, username VARCHAR(255) NOT NULL);")
+            self.cur.execute("CREATE TABLE IF NOT EXISTS users_info (user_id VARCHAR(50) PRIMARY KEY, level INTEGER NOT NULL, exp INTEGER NOT NULL);")
+            self.cur.execute("CREATE TABLE IF NOT EXISTS users_winrate (user_id VARCHAR(50) PRIMARY KEY, wins INTEGER NOT NULL, losses INTEGER NOT NULL, dpa FLOAT NOT NULL);")
+            self.cur.execute("CREATE TABLE IF NOT EXISTS users_interests (user_id VARCHAR(50), interest VARCHAR(255) NOT NULL);")
+            self.cur.execute("CREATE TABLE IF NOT EXISTS users_elo (user_id VARCHAR(50) PRIMARY KEY, elo INTEGER NOT NULL);")
             print("Databases created successfully")
         except ProgrammingError as e:
             print("Error creating tables:", e)
@@ -53,7 +53,7 @@ class database_conn:
         responses = self.cur.fetchall()
         return responses
 
-    def add_user_login(self, user_id: int, username: str):
+    def add_user_login(self, user_id: str, username: str):
         """
         Add user login information, any duplicate user_ids will be updated
 
@@ -73,7 +73,7 @@ class database_conn:
             self.cur.execute("UPDATE users_login SET username = %s WHERE user_id = %s;", (username, user_id))
         self.conn.commit()
 
-    def add_user_elo(self, user_id: int, elo: int):
+    def add_user_elo(self, user_id: str, elo: int):
         """
         Add user elo information, any duplicataes user_ids will be updated instead
 
@@ -90,7 +90,7 @@ class database_conn:
         self.conn.commit()
 
 
-    def add_user_info(self, user_id: int, level: int, exp: int):
+    def add_user_info(self, user_id: str, level: int, exp: int):
         """
         Add user information, any duplicate user_ids will be updated.
 
@@ -110,7 +110,7 @@ class database_conn:
             self.cur.execute("UPDATE users_info SET level = %s, exp = %s WHERE user_id = %s;", (level, exp, user_id))
         self.conn.commit()
 
-    def add_user_winrate(self, user_id: int, wins: int, losses: int, dpa: float):
+    def add_user_winrate(self, user_id: str, wins: int, losses: int, dpa: float):
         """
         Add user winrate information, any duplicate user_ids will be updated instead
 
@@ -131,7 +131,7 @@ class database_conn:
             self.cur.execute("UPDATE users_winrate SET wins = %s, losses = %s, dpa = %s WHERE user_id = %s;", (wins, losses, dpa, user_id))
         self.conn.commit()
 
-    def add_user_interest(self, user_id: int, interest: str):
+    def add_user_interest(self, user_id: str, interest: str):
         """
         Add user interest, any duplicates will be ignored
 
@@ -147,7 +147,7 @@ class database_conn:
             self.conn.commit()
 
 
-    def get_user_login(self, user_id: int):
+    def get_user_login(self, user_id: str):
         """
         Get user login information.
 
@@ -160,7 +160,7 @@ class database_conn:
         response = self.cur.fetchone()
         return response
 
-    def get_user_info(self, user_id: int) -> tuple:
+    def get_user_info(self, user_id: str) -> tuple:
         """
         Get user information.
 
@@ -173,13 +173,13 @@ class database_conn:
         response = self.cur.fetchone()
         return response
 
-    def get_user_elo(self, user_id: int) -> tuple:
+    def get_user_elo(self, user_id: str) -> tuple:
         # do something here
         self.cur.execute("SELECT elo FROM users_elo WHERE user_id = %s;", (user_id,))
         response = self.cur.fetchone()
         return response
 
-    def get_user_winrate(self, user_id: int) -> tuple:
+    def get_user_winrate(self, user_id: str) -> tuple:
         """
         Get user winrate information.
 
@@ -192,7 +192,7 @@ class database_conn:
         response = self.cur.fetchone()
         return response
 
-    def get_user_interests(self, user_id: int) -> list:
+    def get_user_interests(self, user_id: str) -> list:
         """
         Get user interests.
         Parameters:
@@ -200,7 +200,7 @@ class database_conn:
         Returns:
             list: A list of tuples containing the user interests.
         """
-        self.cur.execute("SELECT * FROM users_interests WHERE user_id = %s;", (user_id,))
+        self.cur.execute("SELECT interest FROM users_interests WHERE user_id = %s;", (user_id,))
         response = self.cur.fetchall()
         return response
 
